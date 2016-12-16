ece139ff76d6d33605195fc94c28aad636c3a482
Robert <robert0714@gmail.com>
2016-12-15 21:44:27 -0600

    Update bootstrap_ansible.sh

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -5,15 +5,17 @@ set -e
 echo "Installing Repository..."
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
-
+ 
  
 
 echo "Updating Ubuntu ..."
 apt-get update -y
 
 echo "Installing Ansible..."
-apt-get install -y   ansible
-
+#apt-get install -y   ansible
+apt-get install -y python-pip
+pip install -U pip
+pip install ansible 
 
 
 
